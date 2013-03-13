8582a0c30c649f27b08152824807fa98237fe9bd
S Anand <root.node@gmail.com>
2013-03-13 07:16:44 +0000

    Added Unicode support

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import codecs
+import cStringIO
 import urllib
 import hashlib
 import itertools
@@ -48,9 +50,36 @@ def people(state, city, dist):
             cells[-1] = cells[-1].replace(',', '')
             yield cells
 
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        data = self.encoder.encode(data)
+        self.stream.write(data)
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
 if __name__ == '__main__':
     fp = open('gas.csv', 'w')
-    out = csv.writer(fp, lineterminator='\n')
+    out = UnicodeWriter(fp, lineterminator='\n')
     header = 'State,City,Dist,S.No,Consumer No,Consumer Name,Address,Refills,Before,After-Subsidized,After-Non-Subsidized,Subsidy Rs'.split(',')
     out.writerow(header)
     for state_id, state_name in states():
